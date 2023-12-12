df5e436675d10f1780fcb815e243e0b1e12a1840
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-12 16:34:55 -0500

    Remove sonar secrets variable inside main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -209,16 +209,6 @@ resource "helm_release" "sonarqube" {
   depends_on = [helm_release.prometheus_operator_crds]
   timeout    = var.helm_timeout
 
-  set_sensitive {
-    name  = "sonar.login"
-    value = var.sonarcloud_token
-  }
-
-  set {
-    name  = "sonar.projectKey"
-    value = var.sonarcloud_project_key
-  }
-
   values = [
     <<-EOF
     initSysctl:
